2f42ee4e4632dd89bfaf3f5a4b51c91590372412
NJNataren <nj.nataren@gmail.com>
2024-04-04 12:10:15 +1030

    Update_by_NJNatren_to use_RSeQC_module_read_distribution.sh
    Commented out explicit calling rseqc using minconda3 python, can now just call read_distribution.py directly as rseqc module is loaded by the 'read_distribution_run_parallel.pbs' script. Tested and working.

--- Scripts/read_distribution.sh
+++ Scripts/read_distribution.sh
@@ -35,4 +35,6 @@ echo "$(date): Running RSeQC's read_distribution.py. Sample ID:${sampleid}, BAM:
 
 # Below I am calling the read_distribution.py from the rseqc package, using the python command for the miniconda3 python version 
 # used by the conda rnaseq-de environment
-python /home/567/nn8573/miniconda3/pkgs/rseqc-5.0.3-py310h4b81fae_0/bin/read_distribution.py -i ${bam} -r ${bed} > ${out} 2>>${logfile}
+# python /home/567/nn8573/miniconda3/pkgs/rseqc-5.0.3-py310h4b81fae_0/bin/
+# (Commented line above only needed if calling read_distribution.py directly using minicoda3's python) and not the rseqc module
+read_distribution.py -i ${bam} -r ${bed} > ${out} 2>>${logfile}
