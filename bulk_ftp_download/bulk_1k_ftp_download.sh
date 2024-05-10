db621fd12c5eba4225132b69d78946d7279da050
NJNataren <nj.nataren@gmail.com>
2024-05-10 17:11:14 +0930

    Update bulk_1k_ftp_download.sh
    Corrected the column that awk prints to correct one containing the urls.

--- bulk_ftp_download/bulk_1k_ftp_download.sh
+++ bulk_ftp_download/bulk_1k_ftp_download.sh
@@ -5,6 +5,6 @@ set -e
 # Specify the manifest file to be used and run with bash command on Gadi i.e., $bash bulk_1k-ftp_download.sh
 # Replace <population_manifest.txt> with the desired _manifest.txt file e.g., ceph_test_manifest file
 # and replace <population> with the relevant population i.e., CEPH_test
-cat <population_manifest.txt> | awk '{print $3}' | tail -n +2  > <population>_url.txt
+cat <population_manifest.txt> | awk '{print $4}' | tail -n +2  > <population>_url.txt
 
 wget -i <population>_url.txt
